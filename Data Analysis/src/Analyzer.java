bdbfbb3cd17a8ed0159358959932ba3ed68da735
mrmikemcguire <mcguire@mullenhigh.com>
2014-09-06 11:32:44 -0600

    Adding methods

--- Data Analysis/src/Analyzer.java
+++ Data Analysis/src/Analyzer.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 public class Analyzer
 	{
 	private static int qqqCount = 0;
+	private static int length6Count = 0;
 	
 	public static void main(String[] args)
 		{
 		Data.addData();
 		countRecords();
 		findQQQs();
+		countLength6Lettercodes();
 		}
 	
 		public static void countRecords()
@@ -27,8 +29,16 @@ public class Analyzer
 				}
 			System.out.println("There are " + qqqCount + " occurances of \"qqq\" in the data.");
 			}
-
-
-		}
+		
+		public static void countLength6Lettercodes()
+			{
+			for (int i = 0; i < Data.data.size(); i++)
+				{
+				if (Data.data.get(i).getLetterCode().length() == 6)
+					length6Count++;
+				}
+			System.out.println("There are " + length6Count + " lettercodes of length six.");
+			}
+	}
 
 	
