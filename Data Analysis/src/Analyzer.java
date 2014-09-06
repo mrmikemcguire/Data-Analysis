f5870a13eca7a5c5144ae040cb8f419870a00b37
mrmikemcguire <mcguire@mullenhigh.com>
2014-09-06 11:10:58 -0600

    Put ArrayList and data in Data class

--- Data Analysis/src/Analyzer.java
+++ Data Analysis/src/Analyzer.java
@@ -2,119 +2,23 @@ import java.util.ArrayList;
 
 public class Analyzer
 	{
-	static ArrayList <Record> data = new ArrayList <Record>();
 	private static int qqqCount = 0;
 	
 	public static void main(String[] args)
 		{
-		data.add(new Record("qgqggqq", 177711717));
-		data.add(new Record("qgqqgqq", 177771717));
-		data.add(new Record("ggqggqq", 171171777));
-		data.add(new Record("qgqgggq", 171171117));
-		data.add(new Record("qgqgggg", 717717177));
-		data.add(new Record("qgqgqgg", 717717177));
-		data.add(new Record("ggqgqgg", 711117117));
-		data.add(new Record("ggqgqgq", 177117117));
-		data.add(new Record("qggqqgq", 117717117));
-		data.add(new Record("qggggqq", 777117117));
-		data.add(new Record("qggqqqq", 177711117));
-		data.add(new Record("qqgqqqg", 771777111));
-		data.add(new Record("qqggqqg", 771777177));
-		data.add(new Record("ggggqqg", 771777177));
-		data.add(new Record("gqqqqqg", 777177111));
-		data.add(new Record("gqqggqg", 771117177));
-		data.add(new Record("gqqqgqg", 771117711));
-		data.add(new Record("qgqqgqg", 717717771));
-		data.add(new Record("qgqqggq", 117177711));
-		data.add(new Record("qgqqggq", 717117177));
-		data.add(new Record("qggqqgq", 171777117));
-		data.add(new Record("qggggqq", 171117117));
-		data.add(new Record("qggqqqq", 777111177));
-		data.add(new Record("qgqgggq", 177711717));
-		data.add(new Record("qgqgggg", 717711177));
-		data.add(new Record("qgqgggq", 177717117));
-		data.add(new Record("qgqgggg", 711717177));
-		data.add(new Record("gqqggqg", 771117177));
-		data.add(new Record("gqqqgqg", 771117711));
-		data.add(new Record("qgqqgqg", 717717771));
-		data.add(new Record("qgqqggq", 117177711));
-		data.add(new Record("qgqqggq", 717117177));
-		data.add(new Record("qggqqgq", 171777117));
-		data.add(new Record("qggggqq", 171117117));
-		data.add(new Record("qggqqqq", 777111177));
-		data.add(new Record("gqqggqg", 771117177));
-		data.add(new Record("gqqqgqg", 771117711));
-		data.add(new Record("qgqqgqg", 717717771));
-		data.add(new Record("qgqqggq", 117177711));
-		data.add(new Record("qgqqggq", 717117177));
-		data.add(new Record("qggqqgq", 171777117));
-		data.add(new Record("qggggqq", 171117117));
-		data.add(new Record("qggqqqq", 777111177));
-		data.add(new Record("qgqggqq", 177711717));
-		data.add(new Record("qgqggqq", 177711177));
-		data.add(new Record("qgqggqq", 177717177));
-		data.add(new Record("qgqggqq", 711717177));
-		data.add(new Record("qgqggqq", 177711717));
-		data.add(new Record("qgqggqq", 177711177));
-		data.add(new Record("qgqggqq", 177717177));
-		data.add(new Record("qgqggqq", 711717177));
-		data.add(new Record("qgqqgqq", 177717717));
-		data.add(new Record("ggqggqq", 171171177));
-		data.add(new Record("qgqgggq", 177717117));
-		data.add(new Record("qgqgggg", 711717177));
-		data.add(new Record("qgqgqgg", 717711717));
-		data.add(new Record("ggqgqgg", 711711117));
-		data.add(new Record("ggqgqgq", 171717117));
-		data.add(new Record("qggqqgq", 777717117));
-		data.add(new Record("qggggqq", 171777117));
-		data.add(new Record("qggqqqq", 171777177));
-		data.add(new Record("qqgqqqg", 771111177));
-		data.add(new Record("qqggqqg", 777117177));
-		data.add(new Record("ggggqqg", 771777177));
-		data.add(new Record("gqqqqqg", 771777111));
-		data.add(new Record("gqqggqg", 771177111));
-		data.add(new Record("gqqqgqg", 117177711));
-		data.add(new Record("qgqqgqg", 717111717));
-		data.add(new Record("qgqqggq", 717117711));
-		data.add(new Record("qgqqggq", 717177711));
-		data.add(new Record("qggqqgq", 777717117));
-		data.add(new Record("qggggqq", 171777117));
-		data.add(new Record("qggqqqq", 171777177));
-		data.add(new Record("qgqgggq", 177717117));
-		data.add(new Record("qgqgggg", 711717177));
-		data.add(new Record("qgqgggq", 177711717));
-		data.add(new Record("qgqgggg", 717711177));
-		data.add(new Record("gqqggqg", 771177111));
-		data.add(new Record("gqqqgqg", 117177711));
-		data.add(new Record("qgqqgqg", 717111717));
-		data.add(new Record("qgqqggq", 717117711));
-		data.add(new Record("qgqqggq", 717177711));
-		data.add(new Record("qggqqgq", 777717117));
-		data.add(new Record("qggggqq", 171777117));
-		data.add(new Record("qggqqqq", 171777177));
-		data.add(new Record("gqqggqg", 771177111));
-		data.add(new Record("gqqqgqg", 117177711));
-		data.add(new Record("qgqqgqg", 111717771));
-		data.add(new Record("qgqqggq", 717117177));
-		data.add(new Record("qgqqggq", 717117711));
-		data.add(new Record("qggqqgq", 171717117));
-		data.add(new Record("qggggqq", 777117117));
-		data.add(new Record("qggqqqq", 171777111));
-		data.add(new Record("qgqggqq", 177711177));
-		data.add(new Record("qgqggqq", 177717177));
-		data.add(new Record("qgqggqq", 711717177));
-		data.add(new Record("qgqggqq", 177711717));
-		data.add(new Record("qgqggqq", 177711177));
-		data.add(new Record("qgqggqq", 177717177));
-		data.add(new Record("qgqggqq", 711717177));
-		
+		Data.addData();
+		countRecords();
 		findQQQs();
-		
 		}
+	
+		public static void countRecords()
+			{
+			System.out.println("There are " + Data.data.size() + " records in the database.");
+			}	
 		
 		public static void findQQQs()
 			{
-			for (Record r : data)
+			for (Record r : Data.data)
 				{
 				if (r.getLetterCode().contains("qqq"))
 					{
